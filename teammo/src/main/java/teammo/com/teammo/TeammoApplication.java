2630e08d4b313d48f7bea24867ee3cd36d87003e
seo0o519 <seoyoung344@naver.com>
2024-07-20 12:41:42 +0900

    feat: initial setting

---
+++ teammo/src/main/java/teammo/com/teammo/TeammoApplication.java
@@ -0,0 +1,13 @@
+package teammo.com.teammo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammoApplication.class, args);
+	}
+
+}
---
+++ teammo/src/test/java/teammo/com/teammo/TeammoApplicationTests.java
@@ -0,0 +1,13 @@
+package teammo.com.teammo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeammoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
