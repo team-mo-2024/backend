c00a59cf392e345ca71046c67309a2dcd57a51d2
seo0o519 <seoyoung344@naver.com>
2024-07-25 22:45:38 +0900

    Feat: apart table 관련 코드 작성

---
+++ teammo/src/main/java/teammo/com/teammo/model/Apartment.java
@@ -0,0 +1,45 @@
+package teammo.com.teammo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="apartment")
+public class Apartment {
+
+    @Id
+    private String id_code;
+
+    @Column(name = "apartment_name", nullable = false, length = 45)
+    @JsonProperty("apartment_name")
+    private String apartment_name;
+
+    @Column(name = "address", nullable = false, length = 100)
+    @JsonProperty("address")
+    private String address;
+
+    public String getIdCode() {
+        return id_code;
+    }
+
+    public void setIdCode(String id_code) {
+        this.id_code = id_code;
+    }
+
+    public String getApartmentName() {
+        return apartment_name;
+    }
+
+    public void setApartmentName(String apartment_name) {
+        this.apartment_name = apartment_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
