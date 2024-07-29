6ecda333f6ee475303d7e524f6c68cb2f4d2018f
seo0o519 <seoyoung344@naver.com>
2024-07-29 23:35:50 +0900

    Feat: 게시물 전체 조회, 작성 기능 구현

---
+++ teammo/src/main/java/teammo/com/teammo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package teammo.com.teammo.repository;
+
+import teammo.com.teammo.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
